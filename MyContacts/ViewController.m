ee6d453589d8ce17a280e5615236cb234b3f2fa2
Wiktor Biruk <wibiru@emergencys-Mac-mini.local>
2015-07-16 14:58:03 +0200

    Wyświetla tylko imiona

--- MyContacts/ViewController.m
+++ MyContacts/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import AddressBook;
 
 @interface ViewController ()
 
@@ -14,9 +15,58 @@
 
 @implementation ViewController
 
+NSMutableArray *allContactsAsStrings;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, nil);
+    //ABRecordRef person;
+    NSString *personAsString;
+    
+
+    NSArray *allContacts = (__bridge NSArray *)ABAddressBookCopyArrayOfAllPeople(addressBook);
+    allContactsAsStrings = [[NSMutableArray alloc] init];
+    for (int i = 0; i < [allContacts count]; i++){
+        
+        //person = (__bridge ABRecordRef)allContacts[i];
+        //personAsString = (__bridge_transfer NSString *)ABRecordCopyValue(person, kABPersonFirstNameProperty);
+        //[personAsString stringByAppendingString:(__bridge_transfer NSString *)ABRecordCopyValue(person, kABPersonLastNameProperty)];
+        //[allContactsAsStrings addObject:personAsString];
+        
+        //[allContactsAsStrings addObject:(__bridge_transfer NSString *)ABRecordCopyValue((__bridge ABRecordRef)allContacts[i], kABPersonFirstNameProperty)];
+        
+        
+       
+        personAsString = (__bridge_transfer NSString *)ABRecordCopyValue((__bridge ABRecordRef)allContacts[i], kABPersonFirstNameProperty);
+        [personAsString stringByAppendingFormat:@" %@", (__bridge_transfer NSString *)ABRecordCopyValue((__bridge ABRecordRef)allContacts[i], kABPersonLastNameProperty)];
+        [allContactsAsStrings addObject:personAsString];
+        
+        
+        
+    }
+    
+    
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [allContactsAsStrings count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [allContactsAsStrings objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,3 +75,5 @@
 }
 
 @end
+
+
